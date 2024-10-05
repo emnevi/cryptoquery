0f67279342879c87eb01fc7681c94f3cdedbb48d
Nev <nevikhun@gmail.com>
2024-10-05 11:24:48 +0200

    update

--- src/app/navbar.js
+++ src/app/navbar.js
@@ -7,9 +7,6 @@ export default function Navbar() {
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
         <a className="navbar-brand" href="#">CrypTools</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
